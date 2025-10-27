99cf0cc36b8db57996a283415b1bb2f9bd875468
[dyad] <kh.alkot@gmail.com>
2025-10-27 14:27:44 -0700

    [dyad] تصحيح استيراد صفحة الرحلات المقبولة للسائق في App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import IndexPage from './pages/Index';
 import AuthPage from './pages/AuthPage';
 import PassengerDashboard from './pages/PassengerDashboard';
 import DriverDashboard from './pages/DriverDashboard';
-import PassengerRequestsPage from './pages/PassengerRequestsPage'; // Corrected import
+import PassengerRequestsPage from './pages/PassengerRequestsPage';
 // import RequestRidePage from './pages/RequestRidePage'; // Removed import
 import FindRidesPage from './pages/FindRidesPage';
-import AcceptedRidesPage from './pages/AcceptedRidesPage';
+import DriverAcceptedRidesPage from './pages/DriverAcceptedRidesPage'; // Corrected import
 import RideDetailsPage from './pages/RideDetailsPage';
 import DriverProfilePage from './pages/DriverProfilePage';
 import PassengerProfilePage from './pages/PassengerProfilePage';
@@ -28,14 +28,14 @@ function App() {
 
             {/* Passenger Routes */}
             <Route path="/passenger-dashboard" element={<ProtectedRoute><PassengerDashboard /></ProtectedRoute>} />
-            <Route path="/passenger-dashboard/my-rides" element={<ProtectedRoute><PassengerRequestsPage /></ProtectedRoute>} /> {/* Corrected usage */}
+            <Route path="/passenger-dashboard/my-rides" element={<ProtectedRoute><PassengerRequestsPage /></ProtectedRoute>} />
             {/* <Route path="/request-ride" element={<ProtectedRoute><RequestRidePage /></ProtectedRoute>} /> Removed route */}
             <Route path="/passenger-dashboard/profile" element={<ProtectedRoute><PassengerProfilePage /></ProtectedRoute>} />
 
             {/* Driver Routes */}
             <Route path="/driver-dashboard" element={<ProtectedRoute><DriverDashboard /></ProtectedRoute>} />
             <Route path="/driver-dashboard/find-rides" element={<ProtectedRoute><FindRidesPage /></ProtectedRoute>} />
-            <Route path="/driver-dashboard/accepted-rides" element={<ProtectedRoute><AcceptedRidesPage /></ProtectedRoute>} />
+            <Route path="/driver-dashboard/accepted-rides" element={<ProtectedRoute><DriverAcceptedRidesPage /></ProtectedRoute>} /> {/* Corrected usage */}
             <Route path="/ride-details/:rideId" element={<ProtectedRoute><RideDetailsPage /></ProtectedRoute>} />
             <Route path="/driver-dashboard/profile" element={<ProtectedRoute><DriverProfilePage /></ProtectedRoute>} />
           </Routes>
