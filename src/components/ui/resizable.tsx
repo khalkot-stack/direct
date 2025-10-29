41a3d752453a3900c07bbf12c51d13c3319e4645
[dyad] <kh.alkot@gmail.com>
2025-10-29 15:26:25 -0700

    [dyad] إصلاح أخطاء استيراد مكونات Resizable عن طريق الاستيراد المباشر - wrote 1 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,13 +1,9 @@
 "use client"
 
 import {
-  ResizablePanelGroup as ResizablePanelGroupPrimitive,
-  ResizablePanel as ResizablePanelPrimitive,
-  ResizableHandle as ResizableHandlePrimitive,
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
 } from "react-resizable-panels"
-
-const ResizablePanelGroup = ResizablePanelGroupPrimitive
-const ResizablePanel = ResizablePanelPrimitive
-const ResizableHandle = ResizableHandlePrimitive
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
