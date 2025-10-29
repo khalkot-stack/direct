3499784d4b4a1161897fc20dbe265983e14adb35
[dyad] <kh.alkot@gmail.com>
2025-10-29 15:25:37 -0700

    [dyad] إعادة تأكيد تنفيذ مكونات Resizable القياسية واقتراح إعادة بناء التطبيق - wrote 1 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import {
-  ResizablePanelGroup,
-  ResizablePanel,
-  ResizableHandle,
+  ResizablePanelGroup as ResizablePanelGroupPrimitive,
+  ResizablePanel as ResizablePanelPrimitive,
+  ResizableHandle as ResizableHandlePrimitive,
 } from "react-resizable-panels"
+
+const ResizablePanelGroup = ResizablePanelGroupPrimitive
+const ResizablePanel = ResizablePanelPrimitive
+const ResizableHandle = ResizableHandlePrimitive
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
