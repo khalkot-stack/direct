91c787dec5923fc19bcb27ddd3be9dfb827ab219
[dyad] <kh.alkot@gmail.com>
2025-11-03 15:55:39 -0800

    [dyad] Added global module declaration for Leaflet to fix type resolution. - wrote 1 file(s)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,7 @@
 /// <reference types="leaflet" />
 /// <reference types="react-leaflet" />
 
+// Explicitly declare the 'leaflet' module to help TypeScript find its types.
+declare module 'leaflet';
+
 // If you need to add custom types for Leaflet or other libraries, add them here.
