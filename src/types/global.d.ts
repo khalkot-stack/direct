d4510799aca556dd10fdd92e73205d24f3781a2e
[dyad] <kh.alkot@gmail.com>
2025-11-03 16:10:24 -0800

    [dyad] Added explicit module declaration for Leaflet types in global.d.ts. - wrote 1 file(s)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="leaflet" />
 /// <reference types="react-leaflet" />
 
-// If you need to add custom types for Leaflet or other libraries, add them here.
+// Explicitly declare the 'leaflet' module and re-export its types.
+// This helps TypeScript correctly resolve the module and merge with the installed @types/leaflet package,
+// preventing "Could not find a declaration file" errors and allowing access to types like L.LatLngExpression.
+declare module 'leaflet' {
+  export * from 'leaflet';
+}
+
+// If you need to add custom types for other libraries, add them here.
